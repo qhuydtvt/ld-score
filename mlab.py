4909febd55479b3a51446ab8dde25e5269ca449f
huynq <qhuydtvt@gmail.com>
2017-12-05 03:17:06 +0700

    Change datetime to ISO8061 format

--- mlab.py
+++ mlab.py
@@ -1,4 +1,5 @@
 import mongoengine
+from bson.json_util import DatetimeRepresentation, JSONOptions, DEFAULT_JSON_OPTIONS
 
 #mongodb://<dbuser>:<dbpassword>@ds129946.mlab.com:29946/ld-score
 
@@ -8,15 +9,17 @@ db_name = "ld-score"
 user_name = "admin"
 password = "codethechange123"
 
+options = DEFAULT_JSON_OPTIONS
+options.datetime_representation = DatetimeRepresentation.ISO8601
 
 def mlab_connect():
     mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
 
 def list2json(l):
     import json
-    return [json.loads(item.to_json()) for item in l]
+    return [json.loads(item.to_json(json_options=options)) for item in l]
 
 
 def item2json(item):
     import json
-    return json.loads(item.to_json())
+    return json.loads(item.to_json(json_options=options))
